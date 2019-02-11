b0619a36297caf7eb77b162e357f81fa9fa7a30f
Trahji95 <41341845+Trahji95@users.noreply.github.com>
2019-02-11 17:36:50 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 16:15:15 2019
+
+@author: student
+"""
+
+def numbers_message():
+    print("123156146851651")
+
+
+# Making Functions more powerful with parameters
+
+    
+def say_hi(name):
+    print("Hello "+name+".")
+    
+say_hi("name")
+
+# The return statement
+
+def cube(num):
+    return num*num*num
+
+print(cube(3))
+number = cube(4)
+
+
