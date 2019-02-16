b9a8be88d84ede625a3ea925458eb795ba143cf9
Trahji95 <41341845+Trahji95@users.noreply.github.com>
2019-02-16 16:12:44 -0500

    Add files via upload

---
+++ for loops.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 16:03:46 2019
+
+@author: student
+"""
+
+# For Loops
+
+for letter in "Giraffe Academy":
+    print(letter)
+friends = ["Jin","Baka", "Mazuf"]
+for friend in friends:
+    print(friend)
+for index in range(10):
+    print(index)
+for index in range(3,10):
+    print(index)
+for index in range(len(friends)):
+    print(friends[index])
+for index in range(5):
+    if index==0:
+        print("first iteration")
+    else:
+        print("not first")
+
